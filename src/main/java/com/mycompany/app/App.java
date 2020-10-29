0cb1ff2a613ff9d58afb170e326429a09010207b
ghost-goa <73252400+ghost-goa@users.noreply.github.com>
2020-10-29 14:44:44 +0530

    smallchange of version to check

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!,changes for version2.0 by ghost.!" );
+        System.out.println( "Hello World!,changes for version3.0 by ghost.!" );
     }
 }
